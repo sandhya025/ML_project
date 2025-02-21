d8a57e64260f20c016c5088147825970bef30210
Sandhya <sandhya025lko@gmail.com>
2025-02-21 07:35:19 +0530

    added logging and exception handling

--- src/logger.py
+++ src/logger.py
@@ -9,3 +9,10 @@ os.makedirs(logs_dir, exist_ok=True)
 LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
 
 logging.basicConfig(filename=LOG_FILE_PATH, level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+if __name__ == "__main__":
+    logging.info("logging has started")
+    logging.error("This is an error message")
+    logging.warning("This is a warning message")
+    logging.debug("This is a debug message")
+    logging.critical("This is a critical message")
